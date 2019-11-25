cafd6d565700f4b4b6e3df9f41063bb51589caff
Alik Khilazhev <alikhil@mail.ru>
2019-11-25 14:07:49 +0300

    Add method for creating converter from file

--- convert.go
+++ convert.go
@@ -1,7 +1,9 @@
 package converter
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
 	"strings"
 )
 
@@ -19,6 +21,35 @@ func (c *Converter) FromEn(msg string) string {
 // ToEn converts from converter layout to eng
 func (c *Converter) ToEn(msg string) string {
 	return c.toReplacer.Replace(msg)
+}
+
+func createFromDict(dict map[string]string) (*Converter, error) {
+	var (
+		full    []string
+		reverse []string
+	)
+	for k, v := range dict {
+		full = append(full, k, v)
+		reverse = append(reverse, v, k)
+
+		full = append(full, strings.ToUpper(k), strings.ToUpper(v))
+		reverse = append(reverse, strings.ToUpper(v), strings.ToUpper(k))
+	}
+	return &Converter{strings.NewReplacer(full...), strings.NewReplacer(reverse...)}, nil
+}
+
+// CreateFromFile creating converter from json file
+func CreateFromFile(path string) (*Converter, error) {
+	var data, err = ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	var dict map[string]string
+	err = json.Unmarshal(data, &dict)
+	if err != nil {
+		return nil, err
+	}
+	return createFromDict(dict)
 }
 
 // Create creates new instance of converter using dictName dictionary
@@ -59,16 +90,6 @@ func Create(dictName string) (*Converter, error) {
 	default:
 		return nil, errors.New("dict not found")
 	}
-	var (
-		full    []string
-		reverse []string
-	)
-	for k, v := range dict {
-		full = append(full, k, v)
-		reverse = append(reverse, v, k)
 
-		full = append(full, strings.ToUpper(k), strings.ToUpper(v))
-		reverse = append(reverse, strings.ToUpper(v), strings.ToUpper(k))
-	}
-	return &Converter{strings.NewReplacer(full...), strings.NewReplacer(reverse...)}, nil
+	return createFromDict(dict)
 }
